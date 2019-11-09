54c864445d075af980973fed1e2d630c3f00d6f3
root <root@rescue>
2019-11-09 06:19:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const configMensaje = require('./configMensaje');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/newBookAppToUser', (req, res) => {
+	newBookAppToUser(req.body);
+	res.status(200).send;
+})
+app.post('/newBookAppToAdmin', (req, res) => {
+	newBookAppToAdmin(req.body);
+	res.status(200).send;
+})
+app.post('/newValidationAppToUser', (req, res) => {
+	newValidationAppToUser(req.body);
+	res.status(200).send;
+})
+app.post('/newValidationAppToAdmin', (req, res) => {
+	newValidationAppToAdmin(req.body);
+	res.status(200).send;
+})
+app.post('/successValidation', (req, res) => {
+	successValidation(req.body);
+	res.status(200).send;
+})
+app.post('/newContactAppToAdmin', (req, res) => {
+	newContactAppToAdmin(req.body);
+	res.status(200).send;
+})
+app.listen(3000, () => {
+console.log('Servidor corriendo')
+});
